d9c1a5483f7c92f60152503e61438f268bc38b7a
gemtunde <gemtunde@gmail.com>
2023-09-12 08:55:03 +0100

    default layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
 import React from "react";
-import axios from "axios";
-import { cookies } from "next/headers";
 
-export async function getCurrentUser() {
-  try {
-    const cookeStore = cookies();
-    const token = cookeStore.get("token")?.value;
+//import axios from "axios";
+//import { cookies } from "next/headers";
 
-    const response = await axios.get(
-      `${process.env.domain}/api/users/currentuser`,
-      {
-        headers: {
-          Cookie: `token=${token}`,
-        },
-      }
-    );
-    return response.data.data;
-  } catch (error: any) {
-    console.log(error);
-    return error.message;
-  }
-}
+//server side rendring ...its not needed here.. but this how you call it
+// export async function getCurrentUser() {
+//   try {
+//     const cookeStore = cookies();
+//     const token = cookeStore.get("token")?.value;
+
+//     const response = await axios.get(
+//       `${process.env.domain}/api/users/currentuser`,
+//       {
+//         headers: {
+//           Cookie: `token=${token}`,
+//         },
+//       }
+//     );
+//     return response.data.data;
+//   } catch (error: any) {
+//     console.log(error);
+//     return error.message;
+//   }
+// }
 
 export default async function Home() {
-  const currentUser = await getCurrentUser();
+  //server side calling
+  //const currentUser = await getCurrentUser();
   return (
     <div>
       <h2>car rental</h2>
-      {currentUser && <h2>welcome : {currentUser.email}</h2>}
+      {/* server side rendering.... not needed here for now */}
+      {/* {currentUser && <h2>welcome : {currentUser.email}</h2>} */}
     </div>
   );
 }
--- src/components/LayoutProvider.tsx
+++ src/components/LayoutProvider.tsx
@@ -1,10 +1,36 @@
 "use client";
-import React from "react";
-import { ConfigProvider } from "antd";
+import React, { useEffect, useState } from "react";
+import { ConfigProvider, message } from "antd";
+import { usePathname, useRouter } from "next/navigation";
+import axios from "axios";
 
 const LayoutProvider = ({ children }: any) => {
+  const router = useRouter();
+  const [user, setUser] = useState<any>(null);
+  const pathname = usePathname();
+
+  const getCurrentUser = async () => {
+    try {
+      const response = await axios.get("/api/users/currentuser");
+      setUser(response.data.data);
+    } catch (error: any) {
+      message.error(error.response.data.message || error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (pathname !== "/login" && pathname !== "/register") {
+      getCurrentUser();
+    }
+  }, []);
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <ConfigProvider
           theme={{
@@ -18,7 +44,22 @@ const LayoutProvider = ({ children }: any) => {
             },
           }}
         >
-          {children}
+          {pathname !== "/login" && pathname !== "/register" && (
+            <div className="header bg-primary flex p-3 items-center justify-between">
+              <h1 className="text-xl text-white uppercase">GemTunde-Cars</h1>
+              <div className="flex gap-5 items-center">
+                <h1
+                  className="text-md text-white underline"
+                  onClick={() => router.push("/profile")}
+                >
+                  {user?.name}
+                </h1>
+                <i className="ri-logout-box-r-line text-white"></i>
+              </div>
+            </div>
+          )}
+
+          <div>{children}</div>
         </ConfigProvider>
       </body>
     </html>
